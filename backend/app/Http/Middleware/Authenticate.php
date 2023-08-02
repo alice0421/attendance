f7c16e4090cd6f0d181fd0c1b5d2d8d439c5e20e
有栖 <sekka.alice@gmail.com>
2023-08-02 14:22:19 +0900

    [add] apiではHTTPヘッダーにAccept：application/jsonがあるもののみ許可するように変更。
    ・RequireJsonミドルウェアを実装。
    ・webとapiルーティングの、CSRF対策ミドルウェアの後、暗黙的結合ミドルウェアの前に上記ミドルウェアを設置。

--- backend/app/Http/Middleware/Authenticate.php
+++ backend/app/Http/Middleware/Authenticate.php
@@ -1,21 +1,30 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Middleware;
 
+use App\Domains\Auth\Domain\Exceptions\UserUnauthorizedException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
+     * @param  \Illuminate\Http\Request $request
+     * @return JsonResponse|null
      */
-    protected function redirectTo($request)
+    protected function redirectTo($request): JsonResponse|null
     {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+        $error = new UserUnauthorizedException();
+        abort(response()->json([
+            'errors' => [
+                'code' => $error->getCode(),
+                'message' => $error->getMessage(),
+            ],
+        ], $error->getCode()));
     }
 }
